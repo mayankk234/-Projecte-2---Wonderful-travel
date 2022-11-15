a28808e392d15b72d39aaf3ffd7c890992964a65
mkashyap777 <kashyapmayank234@gmail.com>
2022-11-15 21:20:38 +0100

    2 dia

--- vista/index.vista.php
+++ vista/index.vista.php
@@ -1,26 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wonderful-Travel</title>
     <!-- stylesheet -->
-    <link rel="stylesheet" href="../css/style.css">
     <!-- scripts -->
     <script src="../js/funcions.js" defer></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-</head>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap" rel="stylesheet">  
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet"/>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="../css/style.css">
+
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-1">
-                <div id="relojAnalogico" class="reloj"></div>
+    <div class="container-12">
+        <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+            <a class="navbar-brand text-light" href="#">Wonderful-Travel</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link text-light" href="../logica/index.php">Home</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav w-100 justify-content-end ">
+                    <li>
+                        <div class="nav-item active dropdown pr-3">
+                            <a class="nav-link dropdown-toggle active text-light" data-bs-toggle="dropdown" aria-expanded="false">
+                                Log In / Register
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item " href="#">Log In</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Register</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
             </div>
-            <div class="col text-start">
-                <h1 class="header">Wonderful  Travel</h1>
+        </nav>
+    </div>
+
+
+    <div class="container">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-2">
+                <h1 class="text-end" id="header1">Wonderful</h1>
+            </div>
+            <div id="relojAnalogico" class=" col-1 reloj text-center"></div>
+            <div class="col-2">
+                <h1>Travel</h1>
             </div>
         </div>
+        <hr>
     </div>
+
 </body>
+
 </html>
