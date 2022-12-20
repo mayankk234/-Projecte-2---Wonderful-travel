4ade287fb6849b274e571f9e67916d9d2240200b
adriangd03 <80117534+adrigd03@users.noreply.github.com>
2022-12-20 20:33:09 +0100

    Final commit
    Commit actualitzant el sql de la carpeta amb la base de daes en el seu estat final.

--- SQL/wonderful_travel.sql
+++ SQL/wonderful_travel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-12-2022 a las 00:29:51
+-- Tiempo de generación: 20-12-2022 a las 20:32:17
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -20,9 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `wonderful_travel`
 --
-DROP DATABASE IF EXISTS `wonderful_travel`;
-CREATE DATABASE IF NOT EXISTS `wonderful_travel`;
-USE `wonderful_travel`;
+
 -- --------------------------------------------------------
 
 --
@@ -42,6 +40,34 @@ INSERT INTO `continents` (`continent`) VALUES
 ('America'),
 ('Asia'),
 ('Europa');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historic`
+--
+
+CREATE TABLE `historic` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `desti` varchar(45) NOT NULL,
+  `preu` int(11) NOT NULL,
+  `nom` varchar(45) NOT NULL,
+  `telefon` int(11) NOT NULL,
+  `persones` int(11) NOT NULL,
+  `cancelat` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `historic`
+--
+
+INSERT INTO `historic` (`id`, `date`, `desti`, `preu`, `nom`, `telefon`, `persones`, `cancelat`) VALUES
+(22, '2022-12-30', 'Tailandia', 1905, 'Aas', 603116026, 3, 1),
+(24, '2022-12-20', 'Senegal', 2460, 'hjadf', 603116026, 3, 0),
+(25, '2022-12-20', 'Bangladesh', 2820, 'uguh', 603116026, 5, 0),
+(26, '2022-12-23', 'China', 3120, 'wrfrgr', 603116026, 6, 0),
+(27, '2022-12-20', 'Alemania', 600, 'aefsfv', 603116026, 6, 0);
 
 -- --------------------------------------------------------
 
@@ -126,6 +152,16 @@ CREATE TABLE `reserves` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Volcado de datos para la tabla `reserves`
+--
+
+INSERT INTO `reserves` (`id`, `date`, `desti`, `preu`, `nom`, `telefon`, `persones`) VALUES
+(24, '2022-12-20', 'Senegal', 2460, 'hjadf', 603116026, 3),
+(25, '2022-12-20', 'Bangladesh', 2820, 'uguh', 603116026, 5),
+(26, '2022-12-23', 'China', 3120, 'wrfrgr', 603116026, 6),
+(27, '2022-12-20', 'Alemania', 600, 'aefsfv', 603116026, 6);
+
+--
 -- Índices para tablas volcadas
 --
 
@@ -134,6 +170,12 @@ CREATE TABLE `reserves` (
 --
 ALTER TABLE `continents`
   ADD PRIMARY KEY (`continent`);
+
+--
+-- Indices de la tabla `historic`
+--
+ALTER TABLE `historic`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `paisos`
@@ -159,10 +201,16 @@ ALTER TABLE `reserves`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `historic`
+--
+ALTER TABLE `historic`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
 -- AUTO_INCREMENT de la tabla `reserves`
 --
 ALTER TABLE `reserves`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- Restricciones para tablas volcadas
@@ -173,6 +221,12 @@ ALTER TABLE `reserves`
 --
 ALTER TABLE `paisos`
   ADD CONSTRAINT `fk_paisos_continents` FOREIGN KEY (`continent`) REFERENCES `continents` (`continent`);
+
+--
+-- Filtros para la tabla `preus`
+--
+ALTER TABLE `preus`
+  ADD CONSTRAINT `fk_preus_paisos` FOREIGN KEY (`pais`) REFERENCES `paisos` (`pais`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
